fbbf5fd4b16104619cbceb4524e566635db45267
Iván Guillén <zeopix@gmail.com>
2012-03-24 15:56:52 +0100

    admin bugfix

--- src/Role/AdminBundle/Controller/DefaultController.php
+++ src/Role/AdminBundle/Controller/DefaultController.php
@@ -6,11 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-use Google\MapsBundle\Entity\AddressComponent;
-use Google\MapsBundle\Geo\Coding;
-use Google\MapsBundle\Form\MapType;
-use Role\AdminBundle\Form\SampleType;
 use Genemu\Bundle\FormBundle\Form\JQuery\Type\GeolocationType;
+
 /**
 * @Route("/admin")
 */
@@ -22,17 +19,6 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-		$form = $this->createForm(new MapType());
-		$request = $this->getRequest();
-		
-		if($request->getMethod() === "post"){
-		$form->bindRequest($request);
-		
-		if($form->isValid()){
-			//despues...
-		}
-		}
-    	    	
-    	return Array('form'=>$form->createView());
+		return new Array();
     }
 }
